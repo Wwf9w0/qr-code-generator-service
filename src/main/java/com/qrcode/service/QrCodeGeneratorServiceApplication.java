d73e09ead8ae742d7b31483b2ad6b80e88f23e93
wwf9w0 <aliemrekaradag1@gmail.com>
2024-07-16 20:29:54 +0300

    first commit

---
+++ src/main/java/com/qrcode/service/QrCodeGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qrcode.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeGeneratorServiceApplication.class, args);
+	}
+
+}
